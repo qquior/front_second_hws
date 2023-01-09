919fe8e06f7b576b69875a283111e7b2981b7b2e
qquior <122113546+qquior@users.noreply.github.com>
2023-01-09 15:12:55 +0600

    выполнила дз2

---
+++ dz2-Malika-22-01/main.js
@@ -0,0 +1,26 @@
+// 1 часть дз
+// lst=[10,20,30,40,50,55,555,125]
+// for (element of lst){
+//     if (String(element)[0]==="1" || String(element)[0]==="2" ||String(element)[0]==="5"){
+//         console.log(element)
+//     }
+// }
+
+
+
+
+// 2 часть дз
+// for (let i=20 ; i>0 ; i-=1){
+//     console.log(i)
+// }
+
+
+
+// 3 часть дз
+// var word=prompt("write the color")
+// var obj = {"red":"stop" , "yellow":"wait" , "green":"go"}
+// if( word in obj ){
+//     console.log(obj[word])
+// }else{
+//     console.log("write this ; yellow , green , red")
+// }

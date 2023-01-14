fd9c7161ad0868ed6a6d0f90f8b0fbac76026317
qquior <122113546+qquior@users.noreply.github.com>
2023-01-14 14:50:42 +0600

    выполнила дз 3

---
+++ dz3_Malika_22_01/main.js
@@ -0,0 +1,24 @@
+// 1 часть дз
+// function newArray(34, 65) {
+//     let array = [];
+//     for (let i = 34; i <= 65; i++) array.push(i);
+//
+//     return array;
+// }
+// 2 часть дз
+// s = prompt()
+// function word(s) {
+//     console.log(s.length)
+// }
+// word(s)
+
+
+// 3 часть дз
+// function calc(a, b) {
+//      console.log(a + b)
+//      console.log(a - b)
+//      console.log(a / b)
+//      console.log(a * b)
+//
+// }
+// calc(38, 2)
